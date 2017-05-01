79f46988ac059a5804e26f5b49d32a622abe6eed
Daniel Lowe <dnllowe@yahoo.com>
2017-04-30 23:50:33 -0400

    basic react routes setup

---
+++ client/react/routes/App.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Home from '../containers/Home'
+
+const App = () => {
+  return (
+    <main>
+      <Switch>
+        <Route path='/' component={ Home } />
+      </Switch>
+    </main>
+  )
+}
+
+export default App
